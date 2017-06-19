7b6b585f7c841cc3c8c20ad84e7c9eaad9743a11
primeval1 <jimmyst@hotmail.gr>
2017-06-20 00:02:40 +0300

    added implementation to return frames between some duration,

--- 6.17/6.17.py
+++ 6.17/6.17.py
@@ -2,30 +2,20 @@ import  numpy as np
 import subprocess as sp
 import shlex
 import json
+import os
 from PIL import Image
 
 #CONSTANTS
 FFMPEG_BIN= 'C:/ffmpeg/bin/ffmpeg.exe'
 FFPROBE_BIN = "C:/ffmpeg/bin/ffprobe.exe"
 VIDEO_PATH = 'C:/Users/Dimitris/PycharmProjects/pikrakis/6.17/media/blocks.mp4'
-
-def experimentalFunction():
-    result = findVideoResolution(VIDEO_PATH)
-
-    command = [FFMPEG_BIN,
-               '-i', VIDEO_PATH,
-               '-f', 'image2pipe',
-               '-pix_fmt', 'rgb24',
-               '-vcodec', 'rawvideo', '-'
-               ]
-    pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10 ** 8)
-    raw_image = pipe.stdout.readline(result[0] * result[1] * 3)
-    # transform the byte read into a numpy array
-    image = np.fromstring(raw_image, dtype='uint8')
-    image = image.reshape((result[0], result[1], 3))
-    img = Image.fromarray(image, 'RGB')
-    img.show()
-    pipe.stdout.flush()
+def count_files(in_directory):
+    joiner= (in_directory + os.path.sep).__add__
+    return sum(
+        os.path.isfile(filename)
+        for filename
+        in map(joiner, os.listdir(in_directory))
+    )
 
 def findVideoResolution(pathToInputVideo):
     cmd = FFPROBE_BIN+" -v quiet -print_format json -show_streams"
@@ -44,13 +34,16 @@ def findVideoResolution(pathToInputVideo):
 
 
 def framesToImages(pathToInputVideo):
-    cmd = FFMPEG_BIN    +" -i "+pathToInputVideo+" -vf select='between(t,2,6)+between(t,15,24)' -vsync 0 out%d.png"
     command = [FFMPEG_BIN,
                '-i', pathToInputVideo,
-               '-vf', "select='between(t,2,6)+between(t,15,24)",
-               '-vsync', '0',
-               '-vcodec', 'rawvideo', '-'
+               '-vf', "select='between(t,0,3)",
+               '-vsync', '0', 'C:/Users/Dimitris/PycharmProjects/pikrakis/6.17/results/out%d.png'
                ]
+    p = sp.Popen(command)
+
+
+framesToImages(VIDEO_PATH)
+
 
 
 
